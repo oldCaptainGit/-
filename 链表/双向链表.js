7a0d879e04696877f12aa815035abb40a699bbc0
lichengshuai <1822942244@qq.com>
2022-08-17 14:57:15 +0800

    double happy

--- 链表/双向链表.js
+++ 链表/双向链表.js
@@ -29,8 +29,83 @@ function DoublyLinkedList() {
     this.length++;
   }
 
+  // 2.toString
+  // 2.1 toString
+  DoublyLinkedList.prototype.toString = () => {
+    return this.backwardString();
+  }
 
-  DoublyLinkedList.prototype.append = () => {
+  // 2.2 forwardToString
+  DoublyLinkedList.prototype.forwardString = () => {
+    let current = this.tail;
+    let resultString = "";
 
+    while (current) {
+      resultString += current.data + " ";
+      current = current.prev;
+    }
+    return resultString;
+  }
+
+  // 2.3 backwardString
+  DoublyLinkedList.prototype.backwardString = () => {
+    let current = this.head;
+    let resultString = "";
+
+    while (current) {
+      resultString += current.data + " ";
+      current = current.next;
+    }
+    return resultString;
+  }
+
+  // 3.insert方法
+  DoublyLinkedList.prototype.insert = (position, newNode) => {
+    if (position < 0 || position > this.length) return;
+
+    if (position == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (position == this.length) {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      var current = this.head;
+      var index = 0;
+      while (index++ < position) {
+        current = current.next;
+      }
+      newNode.next = current;
+      newNode.prev = current.prev;
+      current.prev.next = newNode;
+      current.prev = newNode;
+    }
+  }
+
+  // 4.get方法
+  DoublyLinkedList.prototype.get = (position) => {
+    if (position < 0 || position >= this.length) return null;
+
+    let current = this.head;
+    let index = 0;
+    while (index++ < position) {
+      current = current.next;
+    }
+    return current.data;
+  }
+
+  // 5.indexOf方法
+  DoublyLinkedList.prototype.indexOf = (data) => {
+    var current = this.head;
+    var index = 0;
+    while (current) {
+      if (current.data == data) {
+        return index
+      }
+      current = current.next;
+      index += 1;
+    }
+    return -1
   }
 }
